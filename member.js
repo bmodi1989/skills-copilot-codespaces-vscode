bbbf85b9aa8d582899cb7a77768fcd947a9b43d8
bmodi1989 <127742394+bmodi1989@users.noreply.github.com>
2024-03-26 13:58:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,38 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var skills = document.getElementById("skills");
+    var skillsValue = skills.options[skills.selectedIndex].value;
+    var skillsText = skills.options[skills.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberArray = [];
+    var skillsMemberArrayText = [];
+    var skillsMemberArrayValue = [];
+    var skillsMemberArrayTextValue = [];
+    var skillsMemberArrayTextValueText = [];
+    var skillsMemberArrayTextValueValue = [];
+    var skillsMemberArrayTextValueTextValue = [];
+    var skillsMemberArrayTextValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValue = [];
+    var skillsMemberArrayTextValueValueTextValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValueValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValueValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValueValueValueValue = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValueValueValueValueValueText = [];
+    var skillsMemberArrayTextValueValueTextValueValueValueValueValue
+}
